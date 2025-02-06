4aab4e582561463d0b08d5679d4f16131c6b5c14
j-axis <98724043+j-axis@users.noreply.github.com>
2025-02-05 22:35:31 -0600

    Add files via upload

--- AgePrinter.java
+++ AgePrinter.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class AgePrinter {
+    public static void main (String[] args) {
+        int avgLifespan = 70;
+        int userAge;
+
+        System.out.print("Enter your age: ");
+        Scanner scnr = new Scanner(System.in);
+        userAge = scnr.nextInt();
+        System.out.println(userAge + " is a great age.");
+        System.out.println("Average lifespan is " + avgLifespan);
+    }
 }
